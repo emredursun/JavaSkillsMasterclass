589320ecc0035d0b2856ca6a8f927c7811cf078c
emredursun <info.emredursun@gmail.com>
2024-06-08 19:53:58 +0200

    MethodOverloading in Java

---
+++ src/main/java/com/masterclass/section_01/MethodOverloading.java
@@ -0,0 +1,31 @@
+package com.masterclass.section_01;
+
+public class MethodOverloading {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("New score is " + calculateScore());
+        System.out.println("New score is " + calculateScore(10));
+        System.out.println("New score is " + calculateScore("Tim", 500));
+
+        calculateScore();
+        calculateScore(75);
+        calculateScore("Emre", 100);
+    }
+
+    public static int calculateScore() {
+        return calculateScore(0);
+//        System.out.println("No player name, np player score.");
+    }
+
+    public static int calculateScore(int score) {
+        return calculateScore("Anonymous", score);
+    }
+
+    public static int calculateScore(String playerName, int score) {
+        System.out.println("Player " + playerName + " scored " + score + " points");
+        return score * 1000;
+    }
+
+}
---
+++ src/test/java/com/masterclass/section_01/MethodOverloadingTest.java
@@ -0,0 +1,41 @@
+package com.masterclass.section_01;
+
+import org.junit.jupiter.api.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MethodOverloadingTest {
+
+    @Test
+    void testCalculateScoreNoParams() {
+        assertEquals(0, MethodOverloading.calculateScore());
+    }
+
+    @Test
+    void testCalculateScoreWithScore() {
+        assertEquals(10000, MethodOverloading.calculateScore(10));
+        assertEquals(0, MethodOverloading.calculateScore(0));
+        assertEquals(-10000, MethodOverloading.calculateScore(-10));
+    }
+
+    @Test
+    void testCalculateScoreWithPlayerNameAndScore() {
+        assertEquals(500000, MethodOverloading.calculateScore("Tim", 500));
+        assertEquals(0, MethodOverloading.calculateScore("Alex", 0));
+        assertEquals(-100000, MethodOverloading.calculateScore("Chris", -100));
+    }
+
+    @Test
+    void testCalculateScoreOutput() {
+        // Capture the output to validate the print statements
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        MethodOverloading.calculateScore("Emre", 100);
+        assertTrue(outContent.toString().contains("Player Emre scored 100 points"));
+
+        // Reset the standard output
+        System.setOut(System.out);
+    }
+}
