73fe7fac9766b84f4957514de435f12b3a16c7dd
emredursun <info.emredursun@gmail.com>
2024-06-08 17:05:11 +0200

    Optimize DecimalComparator class

--- src/main/java/com/masterclass/section1/DecimalComparator.java
+++ src/main/java/com/masterclass/section1/DecimalComparator.java
@@ -4,8 +4,8 @@ public class DecimalComparator {
 
     public static boolean areEqualByThreeDecimalPlaces(double first, double second) {
 
-        long firstRounded = (long) (first * 1000);
-        long secondRounded = (long) (second * 1000);
-        return firstRounded == secondRounded;
+        return (long) (first * 1000) == (long) (second * 1000);
+//        return Math.round(first * 1000) == Math.round(second * 1000);
+
     }
 }
