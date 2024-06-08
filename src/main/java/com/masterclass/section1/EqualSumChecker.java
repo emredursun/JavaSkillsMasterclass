3a1af4d81eea1a311cba365a794c423588dea35e
emredursun <info.emredursun@gmail.com>
2024-06-08 17:17:31 +0200

    Add EqualSumChecker class and its Test class

---
+++ src/main/java/com/masterclass/section1/EqualSumChecker.java
@@ -0,0 +1,8 @@
+package com.masterclass.section1;
+
+public class EqualSumChecker {
+
+    public static boolean hasEqualSum(int a, int b, int c) {
+        return (a + b) == c;
+    }
+}
---
+++ src/test/java/com/masterclass/section1/EqualSumCheckerTest.java
@@ -0,0 +1,34 @@
+package com.masterclass.section1;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class EqualSumCheckerTest {
+
+    @Test
+    void testEqualSum() {
+        assertTrue(EqualSumChecker.hasEqualSum(1, 1, 2));
+        assertTrue(EqualSumChecker.hasEqualSum(-1, -1, -2));
+        assertTrue(EqualSumChecker.hasEqualSum(0, 0, 0));
+        assertTrue(EqualSumChecker.hasEqualSum(2, -1, 1));
+        assertTrue(EqualSumChecker.hasEqualSum(Integer.MAX_VALUE, -1, Integer.MAX_VALUE - 1));
+    }
+
+    @Test
+    void testNotEqualSum() {
+        assertFalse(EqualSumChecker.hasEqualSum(1, 1, 3));
+        assertFalse(EqualSumChecker.hasEqualSum(-1, -1, -3));
+        assertFalse(EqualSumChecker.hasEqualSum(2, -1, 0));
+        assertFalse(EqualSumChecker.hasEqualSum(Integer.MAX_VALUE, 1, Integer.MAX_VALUE + 2));
+    }
+
+    @Test
+    void testEdgeCases() {
+        assertTrue(EqualSumChecker.hasEqualSum(Integer.MIN_VALUE, Integer.MAX_VALUE, -1));
+        assertFalse(EqualSumChecker.hasEqualSum(Integer.MIN_VALUE, Integer.MAX_VALUE, 0));
+        assertTrue(EqualSumChecker.hasEqualSum(Integer.MIN_VALUE, 0, Integer.MIN_VALUE));
+        assertTrue(EqualSumChecker.hasEqualSum(Integer.MIN_VALUE, 1, Integer.MIN_VALUE + 1));
+    }
+}
+
