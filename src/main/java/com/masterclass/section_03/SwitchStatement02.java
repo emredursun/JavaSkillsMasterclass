32a6436a43ad808e6c24901cdc25064acd5e7a9c
emredursun <info.emredursun@gmail.com>
2024-06-21 17:36:24 +0200

    Modify SwitchStatement02

--- src/main/java/com/masterclass/section_03/SwitchStatement02.java
+++ src/main/java/com/masterclass/section_03/SwitchStatement02.java
@@ -2,23 +2,30 @@ package com.masterclass.section_03;
 
 public class SwitchStatement02 {
 
-    private static final String str = "Value was ";
+    private static final String str = "th month is ";
 
     public static void main(String[] args) {
 
         int switchValue = 7;
-        System.out.println(str + getValue(switchValue));
+        System.out.println(switchValue + str + getMonthName(switchValue));
 
     }
 
-    public static int getValue(int value) {
+    public static String getMonthName(int value) {
         return switch (value) {
-            case 1 -> 1;
-            case 2 -> 2;
-            case 3 -> 3;
-            case 4 -> 4;
-            case 5 -> 5;
-            default -> 0;
+            case 1 -> "January";
+            case 2 -> "February";
+            case 3 -> "March";
+            case 4 -> "April";
+            case 5 -> "May";
+            case 6 -> "June";
+            case 7 -> "July";
+            case 8 -> "August";
+            case 9 -> "September";
+            case 10 -> "October";
+            case 11 -> "November";
+            case 12 -> "December";
+            default -> "Invalid month number entered!";
         };
     }
 
